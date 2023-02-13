fd36e82ef9ac454abb0b0794a721f66e770460df
Nick Smith <ndaman@gmail.com>
2023-02-13 15:58:51 -0800

    modified:   nvim/lua/plugins/supertab.lua moved helper function to top of file, probably a useless change

--- nvim/lua/plugins/supertab.lua
+++ nvim/lua/plugins/supertab.lua
@@ -1,3 +1,10 @@
+-- helper function to check if there are words before cursor
+local has_words_before = function()
+  unpack = unpack or table.unpack
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
 return {
   -- Use <tab> for completion and snippets (supertab)
   -- first: disable default <tab> and <s-tab> behavior in LuaSnip
@@ -15,12 +22,6 @@ return {
     },
     ---@param opts cmp.ConfigSchema
     opts = function(_, opts)
-      local has_words_before = function()
-        unpack = unpack or table.unpack
-        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-      end
-
       local luasnip = require("luasnip")
       local cmp = require("cmp")
 
